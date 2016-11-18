69885cb69b6b5ab80bc0726167901b90f6ef1fe7
Tomasz Nanowski <tomasz.nanowski@gmail.com>
2016-11-18 02:21:34 +0100

    Added ping to servers

--- main.go
+++ main.go
@@ -5,17 +5,36 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
+	"sort"
+	"time"
+
+	fastping "github.com/tatsushid/go-fastping"
 )
 
 type Server struct {
-	Name     string
-	IPAdress string `json:"ip_address"`
-	Country  string
-	Load     int
+	Name      string
+	IPAddress string `json:"ip_address"`
+	Load      int
+	RTT       time.Duration
 }
 
-func getServers(url string) (servers []Server) {
+type Servers []Server
+
+func (s Servers) Len() int {
+	return len(s)
+}
+
+func (s Servers) Less(i, j int) bool {
+	return s[i].RTT < s[j].RTT
+}
+
+func (s Servers) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func getServers(url string) (servers Servers) {
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -32,11 +51,45 @@ func getServers(url string) (servers []Server) {
 	return
 }
 
+func getPing(servers *Servers) {
+	ping := fastping.NewPinger()
+	ping.Network("udp")
+
+	ping.MaxRTT = time.Second
+
+	table := map[string]*Server{}
+
+	for i, server := range *servers {
+		table[server.IPAddress] = &(*servers)[i]
+		ping.AddIP(server.IPAddress)
+	}
+
+	ping.OnRecv = func(addr *net.IPAddr, t time.Duration) {
+		(*table[addr.String()]).RTT = t
+	}
+
+	ping.Run()
+}
+
+func removeUnreachableServers(servers Servers) (result Servers) {
+	for _, server := range servers {
+		if server.RTT != 0 {
+			result = append(result, server)
+		}
+	}
+	return
+}
+
 func main() {
 	url := "https://api.nordvpn.com/server"
 	servers := getServers(url)
 
+	getPing(&servers)
+	sort.Sort(servers)
+
+	servers = removeUnreachableServers(servers)
+
 	for _, server := range servers {
-		fmt.Println(server.IPAdress)
+		fmt.Printf("%-25v %-20v %v %%\n", server.Name, server.RTT, server.Load)
 	}
 }
